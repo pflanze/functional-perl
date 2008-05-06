b9e76aab39cf541169f8cd06b4b328307246b11a
Christian Jaeger <ch@christianjaeger.ch>
2008-05-06 14:16:43 +0200

    Big whitespace/long lines cleanup (and also some comment removal)

--- Chj/xperlfunc.pm
+++ Chj/xperlfunc.pm
@@ -1,6 +1,6 @@
-# Sat Apr 26 16:42:57 2003  Christian Jaeger, christian.jaeger@ethlife.ethz.ch
-# 
-# Copyright 2003 by Christian Jaeger
+# Sat Apr 26 16:42:57 2003  Christian Jaeger, christian at jaeger mine nu
+#
+# Copyright 2003-2008 by Christian Jaeger
 # Published under the same terms as perl itself
 #
 # $Id$
@@ -48,8 +48,10 @@ These are exported by default.
 
 =item Xlstat(path)
 
-These are wrappers around stat; the x* functions die, the X* ones return undef on ENOENT errors (and still croak on other errors like permission problems).
-When successful, they return objects (based on array with the stat return values) with accessor methods.
+These are wrappers around stat; the x* functions die, the X* ones
+return undef on ENOENT errors (and still croak on other errors like
+permission problems).  When successful, they return objects (based on
+array with the stat return values) with accessor methods.
 
 =item xlocaltime() or xlocaltime(unixtime)
 
@@ -80,10 +82,11 @@ Using a pipe, it can still find out if the launch was successful or not.
 
 =item xmvmkdir(first,newplace [,strict])
 
-Move directory 'first' to 'newplace', then create a new empty dir at place 'first'
-with same permissions as 'newplace'. Only root can recreate owners different
-than current user and groups that the user is not part in, of course. If 'strict'
-is true, croaks if it can't recreate all permission details.
+Move directory 'first' to 'newplace', then create a new empty dir at
+place 'first' with same permissions as 'newplace'. Only root can
+recreate owners different than current user and groups that the user
+is not part in, of course. If 'strict' is true, croaks if it can't
+recreate all permission details.
 
 =item pathref = xtmpdir_with_paragon(pathtoexistingdir [,strict])
 
@@ -98,7 +101,7 @@ try to remove the tmpdir if empty.)
 
 =item xlinkunlink $source,$target
 
-Has the same effect as xrename $source,$target except that it 
+Has the same effect as xrename $source,$target except that it
 won't overwrite $target if it already exists. This is done by using
 link and unlink instead of rename.
 
@@ -117,7 +120,8 @@ a previous target. All the same it does the replace atomically.
 
 =item xfileno $string_or_FH
 
-Does work with both filehandles and integer strings. Croaks if it's neither or there's an error.
+Does work with both filehandles and integer strings. Croaks if it's
+neither or there's an error.
 
 =item basename $pathstring
 
@@ -125,11 +129,14 @@ Same as the shell util (or my old Filename function)
 
 =item dirname $pathstring
 
-Same as the shell util (or about as my old FolderOfThisFile function), except that it croaks if dirname of "/" or "." is requested.
+Same as the shell util (or about as my old FolderOfThisFile function),
+except that it croaks if dirname of "/" or "." is requested.
 
 =item xmkdir_p $pathstring
 
-Works like unix's "mkdir -p": return false if the directory already exists, true if it (and, if necessary, it's parent(s)) has(/have) been created, croaks if some error happens on the way.
+Works like unix's "mkdir -p": return false if the directory already
+exists, true if it (and, if necessary, it's parent(s)) has(/have) been
+created, croaks if some error happens on the way.
 
 =item xlink_p $frompath, $topath
 
@@ -248,7 +255,7 @@ sub xlaunch { ## NA: todo: noch nicht fertig, insbesondere geht kommunikation ex
 	exit; # !
     }
 }
-	
+
 sub xsystem {
     @_>0 or croak "xsystem: missing arguments";
     (system @_)>=0
@@ -268,7 +275,8 @@ sub xwaitpid ( $ ; $ ) {
     my ($pid,$flags)=@_;
     defined $flags or $flags= 0;
     my $kid= waitpid $pid,$flags;
-    die "xwaitpid ($pid,$flags): no child process" if $kid<0; # "no such child" but pid -1 is okay right? so..
+    die "xwaitpid ($pid,$flags): no child process" if $kid<0;
+    # ^ "no such child" but pid -1 is okay right? so..
     #$? # hm drop $kid?  build tuple?   ?
     $kid
 }
@@ -277,7 +285,9 @@ sub xxwaitpid ( $ ; $ ) {
     my ($pid,$flags)=@_;
     defined $flags or $flags= 0;
     my $kid= xwaitpid $pid,$flags;
-    $? == 0 or die "xxwaitpid ($pid,$flags): child process terminated with ".exitcode($?);
+    $? == 0
+	or die "xxwaitpid ($pid,$flags): child process terminated with "
+	.exitcode($?);
     $kid
 }
 
@@ -291,7 +301,8 @@ sub xxwait {
     my $kid= wait;
     defined $kid or die "xwait: $!";# when can this happen? EINTR?
     my $status= $?;
-    $status == 0 or die "xxwait: child process $kid terminated with ".exitcode($?);
+    $status == 0
+	or die "xxwait: child process $kid terminated with ".exitcode($?);
     $kid
 }
 
@@ -308,32 +319,50 @@ sub xlinkunlink {
     unlink $_[0]
 	or do {
 	    my $err= "$!";
-	    my $res= unlink $_[1]; # could this ever be dangerous? answer is yes. does it bother me?
+	    my $res= unlink $_[1];
+	    # ^ could this ever be dangerous? answer is yes. does it bother me?
 	    if ($res) {
-		croak "xlinkunlink(".join(", ",@_)."): can't unlink source: $err, so removed target again";
+		croak "xlinkunlink(".join(", ",@_)
+		    ."): can't unlink source: $err, so removed target again";
 	    } else {
-		croak "xlinkunlink(".join(", ",@_)."): can't unlink source: $err, additionally an error occured while trying to unlink the target again: $!";
+		croak "xlinkunlink(".join(", ",@_)
+		    ."): can't unlink source: $err, "
+		    ."additionally an error occured while trying to unlink "
+		    ."the target again: $!";
 	    }
 	};
 }
 
-sub xxcarefulrename { # since xlinkunlink doesn't work for directories, or not always under grsec. But note: it's careful, not guaranteed to be safe.
+# Since xlinkunlink doesn't work for directories, or not always under
+# grsec: (But note: it's careful, not guaranteed to be safe.)
+sub xxcarefulrename {
     @_==2 or croak "xxcarefulrename: wrong number of arguments";
     my ($source,$dest)=@_;
     if (link $source,$dest) {
 	unlink $source
 	or do {
 	    my $err= "$!";
-	    my $res= unlink $dest; # could this ever be dangerous? answer is yes. does it bother me?
+	    my $res= unlink $dest;
+	    # ^ could this ever be dangerous? answer is yes. does it bother me?
 	    if ($res) {
-		croak "xxcarefulrename(".join(", ",@_)."): can't unlink source: $err, so removed target again";
-		# note: no need to goto rename-branch, since the difficult cases either won't come here (grsec hardlink restriction) or won't be helped by rename (non-owned source file in sticky source dir (we're not even talking about the case where both source and target are in sticky dirs..)).
+		croak "xxcarefulrename(".join(", ",@_)
+		    ."): can't unlink source: $err, so removed target again";
+		# note: no need to goto rename-branch, since the
+		# difficult cases either won't come here (grsec
+		# hardlink restriction) or won't be helped by rename
+		# (non-owned source file in sticky source dir (we're
+		# not even talking about the case where both source
+		# and target are in sticky dirs..)).
 	    } else {
-		croak "xxcarefulrename(".join(", ",@_)."): can't unlink source: $err, additionally an error occured while trying to unlink the target again: $!";
+		croak "xxcarefulrename(".join(", ",@_)
+		    ."): can't unlink source: $err, additionally an error "
+		    ."occured while trying to unlink the target again: $!";
 	    }
 	};
     } else {
-	if (lstat $dest) {# (yes, link too already fails if target exists as a dangling symlink)
+	if (lstat $dest) {
+	    # (yes, link too already fails if target exists as a
+	    # dangling symlink)
 	    croak "xxcarefulrename: target '$dest' already exists";
 	}
 	else {
@@ -347,17 +376,20 @@ sub xxcarefulrename { # since xlinkunlink doesn't work for directories, or not a
 sub xlinkreplace {
     @_==2 or croak "xlinkreplace: wrong number of arguments";
     my ($source,$dest)=@_;
-    ## schon wieder dieser huere temporary try mechanismus. sollte ich dringend eine generische func oder ein makro dafür haben theoretisch
+    ## schon wieder dieser temporary try mechanismus. sollte ich dringend eine generische func oder ein makro dafür haben theoretisch
     # nun im gegensatz zu Tempfile.pm brauchen wir kein eval hier. Aber auch das waer ja per func/macro machbar
     my $path;
   TRY: {
 	for (1..3) {
 	    $!=0;
-	    my $rand= int(rand(99999)*100000+rand(99999));# well, weder gut genug für gefahrfälle noch sinnvoll für nongefahrfall.
+	    my $rand= int(rand(99999)*100000+rand(99999));
+	    # well, not good enough for dangerous cases nor reasonable for
+	    # non-dangerous cases?
 	    $path= "$source.tmp$rand~";
 	    last TRY if link $source,$path;
 	}
-	croak "xlinkreplace: failed 3 attempts to create hardlinks from '$source' to e.g. '$path': $!";
+	croak "xlinkreplace: failed 3 attempts to create hardlinks from "
+	    ."'$source' to e.g. '$path': $!";
     }
     rename $path,$dest
       or croak "xlinkreplace: could not rename '$path' to '$dest': $!";
@@ -393,7 +425,6 @@ sub xchdir {
 
 sub xstat {
     my @r;
-#    if (@_==1) 
     @_<=1 or croak "xstat: too many arguments";
     @r= stat(@_ ? @_ : $_);
     @r or croak (@_ ? "xstat: '@_': $!" : "xstat: '$_': $!");
@@ -406,7 +437,6 @@ sub xstat {
 }
 sub xlstat {
     my @r;
-#    if (@_==1) 
     @_<=1 or croak "xlstat: too many arguments";
     @r= lstat(@_ ? @_ : $_);
     @r or croak (@_ ? "xlstat: '@_': $!" : "xlstat: '$_': $!");
@@ -460,7 +490,9 @@ sub Xlstat {
 }
 
 {
-    package Chj::xperlfunc::xstat; ## Alternative zu arrays: hashes, so dass slices à la ->{"dev","ino"} gemacht werden können. Schade kann man nicht alles haben.
+    package Chj::xperlfunc::xstat;
+    ## Alternative to arrays: hashes, so that slices like
+    ## ->{"dev","ino"} could be done? One can't have everything.
     sub dev     { shift->[0] }
     sub ino     { shift->[1] }
     sub mode    { shift->[2] }
@@ -481,10 +513,11 @@ sub Xlstat {
     sub permissions_g { (shift->[2] & 00070) >> 3 }
     sub permissions_o { shift->[2] & 00007 }
     sub permissions_s { (shift->[2] & 07000) >> 9 }
-    sub setuid { !!(shift->[2] & 04000) } # keine lust, is_ vornedranzuhängen. Es ist ein boolean, wenn man das in perl nicht angeben kann schade
+    sub setuid { !!(shift->[2] & 04000) }
+    # ^ no delight putting is_ in front. It is a boolean, if one
+    # cannot declare that in perl then pity
     sub setgid { !!(shift->[2] & 02000) }
     sub sticky { !!(shift->[2] & 01000) }
-    #sub filetype { (shift->[2] & 070000) >> 12 } # 4*3bits    -- buggy, devices and symlinks are the same with this.
     sub filetype { (shift->[2] & 0170000) >> 12 } # 4*3bits
     # guess access rights from permission bits
     # note that these might guess wrong (because of chattr stuff,
@@ -500,15 +533,17 @@ sub Xlstat {
 	} else {
 	    if ($gids) {
 		for my $gid (@$gids) {
-		    #warn "checking gid $gid.."; aaah fuuk: $) gibt string nicht liste
-		    length($gid)==length($gid+0) or Carp::croak "invalid gid argument '$gid' - maybe you forgot to split '\$)'?";
-		    ### todo: what if one is member of group 0, is this special?
+		    length($gid)==length($gid+0)
+			or Carp::croak "invalid gid argument '$gid' - maybe "
+			." you forgot to split '\$)'?";
+		    ## todo: what if one is member of group 0, is this special?
 		    if ($s->[5] == $gid) {
 			if ($s->[2] & (00010 * $mod)) {
 			    #warn "gid yes";
 			    return 1;
 			} else {
-			    # groups stick just like users, so even if others are allowed, we are not
+			    # groups stick just like users, so even if
+			    # others are allowed, we are not
 			    return 0;
 			}
 		    }
@@ -517,7 +552,8 @@ sub Xlstat {
 		#warn "others. mod=$mod, uid=$uid, gids sind @$gids";
 		return !!($s->[2] & (00001 * $mod))
 	    } else {
-		Carp::croak "missing gids argument - might just be a ref to an empty array";
+		Carp::croak "missing gids argument - might just be a ref to "
+		    ."an empty array";
 	    }
 	}
     }
@@ -540,7 +576,8 @@ sub Xlstat {
     sub is_file { Filetype_is_file(shift->filetype) } # call it is_normalfile ?
     sub Filetype_is_dir { shift == 4 }
     sub is_dir { Filetype_is_dir(shift->filetype) }
-    #sub is_symlink { # rather than calling it is_link?  -l sounds like link. but..
+    #sub is_symlink { # rather than calling it is_link?
+    #  -l sounds like link. but..
     sub Filetype_is_link { shift == 10 }
     sub is_link { Filetype_is_link(shift->filetype) }
     sub Filetype_is_socket { shift == 12 }
@@ -568,7 +605,6 @@ sub Xlstat {
     sub equal {
 	my $s=shift;
 	my ($s2)=@_;
-	#defined ($s2) or Carp::croak "equal: missing argument"; does not help debugging if the argument is a number ("Can't call method "dev" without a package or object reference"), so just let it be.
 	# permissions:
 	($s->equal_content($s2)
 	 and $s->mode == $s2->mode
@@ -604,7 +640,9 @@ sub xlocaltime (;$ ) {
 
 
 sub xreadlink {
-    my $res= @_==0 ? readlink : @_==1 ? readlink $_[0] : croak "xreadlink: wrong number of arguments";
+    my $res= @_==0 ? readlink
+	: @_==1 ? readlink $_[0]
+	: croak "xreadlink: wrong number of arguments";
     defined $res or croak @_? "xreadlink @_: $!" : "xreadlink: $!";
     $res
 }
@@ -615,7 +653,7 @@ sub xmkdir_with_paragon {
     my ($owner,$group,$mode)= (xstat $_[1])[4,5,2];
     xmkdir $_[0],0;
     if (! chown $owner,$group,$_[0]){
-        $_[2] and croak "xmvmkdir: could not recreate user or group: $!";
+	$_[2] and croak "xmvmkdir: could not recreate user or group: $!";
     }
     xchmod $mode,$_[0];
 }
@@ -644,7 +682,7 @@ sub xtmpdir_with_paragon {
 	}
     }
     if (! chown $owner,$group,$newname){
-        if ($strict){
+	if ($strict){
 	    rmdir $newname;
 	    croak "xtmpdir_with_paragon: could not recreate user or group: $!";
 	}
@@ -653,23 +691,17 @@ sub xtmpdir_with_paragon {
 	rmdir $newname;
 	croak "xtmpdir_with_paragon: chmod: $!";
     }
-    #bless \ $newname, 'Chj::xperlfunc::tmpdir'; # so it will be removed upon error
-    #return $newname
-    return bless \ $newname, 'Chj::xperlfunc::tmpdir'; # so it will be removed upon error
+    return bless \ $newname, 'Chj::xperlfunc::tmpdir';
+    # ^ so that it will be removed upon error
 }
 
 sub xmvmkdir {
     @_==2 or @_==3 or croak "xmvmkdir: wrong number of arguments";
     xrename $_[0],$_[1];
-    #warn "arg1 ist '$_[1]'";# arg1 ist 'aK3' at /root/extlib/Chj/xperlfunc.pm line 120.
-
-    my ($owner,$group,$mode)= (xstat $_[1])[4,5,2];# Can't call method "xstat" on an undefined value at /root/extlib/Chj/xperlfunc.pm line 121. ????????  ah das war bevor ich die sub definition hinauf schob. Dann rät er also falsch   hmmmmmm. huree indirect object notation kack macht sogar dass infixehoutfix notation nich mehr rech geht.
-    #my ($owner,$group,$mode)= (xstat ($_[1]))[4,5,2];
-
+    my ($owner,$group,$mode)= (xstat $_[1])[4,5,2];
     xmkdir $_[0],0;
-    #warn "mkdir getan, sleep"; sleep 10; ###
     if (! chown $owner,$group,$_[0]){
-        $_[2] and croak "xmvmkdir: could not recreate user or group: $!";
+	$_[2] and croak "xmvmkdir: could not recreate user or group: $!";
     }
     xchmod $mode,$_[0];
 }
@@ -693,14 +725,9 @@ sub xsymlink {
 
 sub xutime {
     @_>=2 or croak "xutime: wrong number of arguments";
-    #my ($atime,$mtime)=(shift,shift);
-    #utime @_
-    #  or croak "xutime @_: $!";  # auch hier, wenn ich selber loope statt viele files an perl geben könnt ich melden bei welchem file es croakt
-    # MANN ISCH PERL EIN GAGA.
     my ($atime,$mtime)=(shift,shift);
     utime $atime,$mtime,@_
       or croak "xutime @_: $!";
-    # OOH, nein es war ein anderer fehler, sorry perl.
 }
 
 sub xkill{
@@ -709,7 +736,8 @@ sub xkill{
       or croak "xkill $sig @_: $!";
 }
 
-sub xeval( $ ) { # meant for string eval only, of course.  HM ps sollte man hier localize of $@ machen?
+sub xeval( $ ) { # meant for string eval only, of course.
+    ## hm ps should one localize $@ here?
     if (defined wantarray) {
 	if (wantarray) {
 	    my @res=eval $_[0];
@@ -755,7 +783,8 @@ sub xsysread ( $ $ $ ; $ ) {
     defined $rv or die "xsysread(".singlequote_many(@_)."): $!";
     $rv
 }
-# ^- ok this is silly (is it?) since I've got Chj::IO::File. But that latter one is not yet complete, I'm debugging xreadline atm.
+# ^- ok this is silly (is it?) since I've got Chj::IO::File. But that
+# latter one is not yet complete, I'm debugging xreadline atm.
 
 
 sub basename ($ ) {
