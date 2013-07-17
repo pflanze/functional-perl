0eed29a7d5614f6391ab11af1691178f6cad03f7
Christian Jaeger <ch@christianjaeger.ch>
2013-07-17 16:02:47 +0100

    handle case of multiple (or missing) Date headers

--- lib/Chj/FP/ArrayUtil.pm
+++ lib/Chj/FP/ArrayUtil.pm
@@ -19,7 +19,10 @@ Chj::FP::ArrayUtil
 package Chj::FP::ArrayUtil;
 @ISA="Exporter"; require Exporter;
 @EXPORT=qw();
-@EXPORT_OK=qw(Array_hashing_uniq);
+@EXPORT_OK=qw(Array_hashing_uniq
+	      Array_fold
+	      min
+	      max);
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict;
@@ -35,6 +38,30 @@ sub Array_hashing_uniq ($;$ ) {
 	 not $s
      } @$ary
     ]
+}
+
+sub Array_fold ($$$) {
+    my ($fn,$start,$ary)=@_;
+    for (@$ary) {
+	$start= &$fn($_,$start);
+    }
+    $start
+}
+		
+sub min {
+    my $x=shift;
+    for (@_) {
+	$x= $_ if $_ < $x
+    }
+    $x
+}
+
+sub max {
+    my $x=shift;
+    for (@_) {
+	$x= $_ if $_ > $x
+    }
+    $x
 }
 
 1
