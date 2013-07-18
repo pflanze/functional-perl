75cf6c9cb713eaab2d91c64e696c78ad9cd5d16c
Christian Jaeger <ch@christianjaeger.ch>
2013-07-18 13:42:53 +0100

    allow fh argument to be an unblessed glob ref, too

--- ftemplate/lib/Chj/xIO.pm
+++ ftemplate/lib/Chj/xIO.pm
@@ -27,13 +27,13 @@ package Chj::xIO;
 use strict;
 
 sub xprint {
-    my $fh= UNIVERSAL::isa($_[0],"IO") ? shift : *STDOUT{IO};
+    my $fh= (ref($_[0]) eq "GLOB" or UNIVERSAL::isa($_[0],"IO")) ? shift : *STDOUT{IO};
     print $fh @_
       or die "printing to $fh: $!"
 }
 
 sub xprintln {
-    my $fh= UNIVERSAL::isa($_[0],"IO") ? shift : *STDOUT{IO};
+    my $fh= (ref($_[0]) eq "GLOB" or UNIVERSAL::isa($_[0],"IO")) ? shift : *STDOUT{IO};
     print $fh @_,"\n"
       or die "printing to $fh: $!"
 }
