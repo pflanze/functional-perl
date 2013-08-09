a7eddcaa468f5286cb19a0e66fe4a05f7e6824a4
Christian Jaeger <ch@christianjaeger.ch>
2013-08-09 20:38:55 +0100

    create HashSet library

---
+++ lib/Chj/FP/HashSet.pm
@@ -0,0 +1,117 @@
+#
+# Copyright 2013 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+# $Id$
+
+=head1 NAME
+
+Chj::FP::HashSet - set operations for hash tables
+
+=head1 SYNOPSIS
+
+ use Chj::FP::ArrayUtil 'array2hashset';
+ use Chj::FP::HashSet; # ":all";
+
+ my $A= array2hashset ["a","b","c"];
+ my $B= array2hashset ["a","c","d"];
+ hashset2array hashset_union($A,$B) # -> ["a","b","c","d"]
+ hashset2array hashset_intersection($A,$B) # -> ["a","c"]
+ hashset2array hashset_difference($A,$B) # -> ["b"]
+ hashset_subset($B,$A) # -> false
+ hashset_subset(+{b=>1},$A) # -> true
+
+=head1 DESCRIPTION
+
+Hashsets are hash tables that are expected to have keys representing
+the values unambiguously (Chj::FP::ArrayUtil::array2hashset will just
+use the stringification).
+
+Note that hashset2array will use the *values* of the hashes, not the
+keys.
+
+=cut
+
+
+package Chj::FP::HashSet;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(hashset2array
+	   hashset_union
+	   hashset_intersection
+	   hashset_difference
+	   hashset_subset);
+@EXPORT_OK=qw(hashset_add_hashset_d);
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict;
+use Chj::TEST;
+
+sub hashset2array ($) {
+    [
+     sort values %{$_[0]}
+    ]
+}
+
+sub hashset_add_hashset_d ($ $) {
+    my ($r,$s)=@_;
+    for (keys %$s) {
+	$$r{$_} = $$s{$_}
+	  unless exists $$r{$_};
+    }
+}
+
+sub hashset_union {
+    my %r;
+    hashset_add_hashset_d(\%r,$_)
+      for @_;
+    \%r
+}
+
+sub hashset_intersection ($ $) {
+    my ($a,$b)=@_;
+    my %r;
+    for (keys %$a) {
+	$r{$_} = $$b{$_}
+	  if exists $$b{$_};
+    }
+    \%r
+}
+
+sub hashset_difference ($ $) {
+    my ($a,$b)=@_;
+    my %r;
+    for (keys %$a) {
+	$r{$_} = $$a{$_}
+	  unless exists $$b{$_};
+    }
+    \%r
+}
+
+sub hashset_subset ($ $) {
+    my ($subset,$set)=@_;
+    my %r;
+    for (keys %$subset) {
+	return 0
+	  unless exists $$set{$_};
+    }
+    1
+}
+
+
+{
+    use Chj::FP::ArrayUtil 'array2hashset';
+    my $A= array2hashset ["a","b","c"];
+    my $B= array2hashset ["a","c","d"];
+    TEST{ hashset2array hashset_union($A,$B) }
+      ["a","b","c","d"];
+    TEST{ hashset2array hashset_intersection($A,$B)}
+      ["a","c"];
+    TEST{ hashset2array hashset_difference($A,$B)}
+      ["b"];
+    TEST{ hashset_subset($B,$A) }
+      0;
+    TEST{ hashset_subset(+{b=>1},$A) }
+      1;
+}
+
+1
