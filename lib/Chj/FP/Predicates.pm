36f83074af8fd7d7d807fe696bf53a1b3857a4ea
Christian Jaeger <ch@christianjaeger.ch>
2014-06-17 00:12:24 +0100

    create

---
+++ lib/Chj/FP/Predicates.pm
@@ -0,0 +1,81 @@
+#
+# Copyright 2014 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+Chj::FP::Predicates
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Useful as predicates for Chj::Struct field definitions.
+
+=cut
+
+
+package Chj::FP::Predicates;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(
+	      stringP
+	      natural0P
+	      naturalP
+	      boolean01P
+	      booleanP
+	      hashP
+	      arrayP
+	      classP
+	 );
+@EXPORT_OK=qw();
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict;
+
+sub stringP ($) {
+    not ref ($_[0]) # relax?
+}
+
+sub natural0P ($) {
+    not ref ($_[0]) # relax?
+      and $_[0]=~ /^\d+\z/
+}
+
+sub naturalP ($) {
+    not ref ($_[0]) # relax?
+      and $_[0]=~ /^\d+\z/ and $_[0]
+}
+
+# strictly 0 or 1
+sub boolean01P ($) {
+    not ref ($_[0]) # relax?
+      and $_[0]=~ /^[01]\z/
+}
+
+# undef, 0, "", or 1
+sub booleanP ($) {
+    not ref ($_[0]) # relax?
+      and (! $_[0]
+	   or
+	   $_[0] eq "1");
+}
+
+
+sub hashP ($) {
+    defined $_[0] and ref ($_[0]) eq "HASH"
+}
+
+sub arrayP ($) {
+    defined $_[0] and ref ($_[0]) eq "ARRAY"
+}
+
+sub classP ($) {
+    my ($cl)=@_;
+    sub ($) {
+	UNIVERSAL::isa ($_[0], $cl);
+    }
+}
+
+
+1
