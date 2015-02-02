4c8aeb7d48f8b88ea840a1d12a088a9cd8f4d72e
Christian Jaeger <ch@christianjaeger.ch>
2015-02-02 01:01:31 +0100

    resolve circular dependency by splitting off xIOUtil
    huh, who is using the routines from xIOUtil anyway again?

--- lib/Chj/xIO.pm
+++ lib/Chj/xIO.pm
@@ -5,7 +5,7 @@
 
 =head1 NAME
 
-Chj::xIO - exception-throwing I/O wrappers and utilities
+Chj::xIO - exception-throwing I/O wrappers (and utilities)
 
 =head1 SYNOPSIS
 
@@ -20,8 +20,7 @@ Chj::xopen modules.
 package Chj::xIO;
 @ISA="Exporter"; require Exporter;
 @EXPORT=qw(xprint xprintln);
-@EXPORT_OK=qw(xgetfile_utf8 xputfile_utf8 xcopyfile_utf8
-	      capture_stdout capture_stdout_);
+@EXPORT_OK=qw(capture_stdout capture_stdout_);
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict; use warnings FATAL => 'uninitialized';
@@ -36,55 +35,6 @@ sub xprintln {
     my $fh= (ref($_[0]) eq "GLOB" or UNIVERSAL::isa($_[0],"IO")) ? shift : *STDOUT{IO};
     print $fh @_,"\n"
       or die "printing to $fh: $!"
-}
-
-
-# better place for these?
-
-use Chj::xopen ":all";
-# ^ well, this voids the point of Chj::xIO (to avoid Chj::IO::*)
-use Chj::FP::Lazy;
-use Chj::FP::List;
-
-sub xgetfile_utf8 ($) {
-    my ($path)=@_;
-    my $in= xopen_read ($path);
-    binmode $in, ":utf8" or die;
-    $in->xcontent
-}
-
-# print, not write, i.e. flatten nested structures out, but don't
-# print parens for lists etc., just print the contained basic types.
-sub xprint_object ($$);
-sub xprint_object ($$) {
-    my ($fh,$v)=@_;
-    if (ref $v) {
-	if (ref($v) eq "ARRAY") {
-	    xprint_object ($fh, $_) for @$v;
-	} elsif (pairP $v) {
-	    xprint_object ($fh, car $v);
-	    xprint_object ($fh, cdr $v);
-	} elsif (promiseP $v) {
-	    xprint_object ($fh, Force $v)
-	} else {
-	    die "don't know how to print a ".ref($v)." ('$v')";
-	}
-    } else {
-	xprint $fh, $v
-    }
-}
-
-sub xputfile_utf8 ($$) {
-    my ($path,$str)=@_;
-    my $out= xopen_write($path);
-    binmode $out, ":utf8" or die;
-    xprint_object ($out, $str);
-    $out->xclose;
-}
-
-sub xcopyfile_utf8 ($$) {
-    my ($src,$dest)=@_;
-    xputfile_utf8 ($dest, xgetfile_utf8 ($src));
 }
 
 
---
+++ lib/Chj/xIOUtil.pm
@@ -0,0 +1,73 @@
+#
+# Copyright 2015 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+Chj::xIOUtil - exception-throwing I/O utilities
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+
+=cut
+
+
+package Chj::xIOUtil;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw();
+@EXPORT_OK=qw(xgetfile_utf8 xputfile_utf8 xcopyfile_utf8);
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict; use warnings FATAL => 'uninitialized';
+
+use Chj::xopen ":all";
+# ^ well, this voids the point of Chj::xIO (to avoid Chj::IO::*)
+use Chj::FP::Lazy;
+use Chj::FP::List;
+
+sub xgetfile_utf8 ($) {
+    my ($path)=@_;
+    my $in= xopen_read ($path);
+    binmode $in, ":utf8" or die;
+    $in->xcontent
+}
+
+# print, not write, i.e. flatten nested structures out, but don't
+# print parens for lists etc., just print the contained basic types.
+sub xprint_object ($$);
+sub xprint_object ($$) {
+    my ($fh,$v)=@_;
+    if (ref $v) {
+	if (ref($v) eq "ARRAY") {
+	    xprint_object ($fh, $_) for @$v;
+	} elsif (pairP $v) {
+	    xprint_object ($fh, car $v);
+	    xprint_object ($fh, cdr $v);
+	} elsif (promiseP $v) {
+	    xprint_object ($fh, Force $v)
+	} else {
+	    die "don't know how to print a ".ref($v)." ('$v')";
+	}
+    } else {
+	xprint $fh, $v
+    }
+}
+
+sub xputfile_utf8 ($$) {
+    my ($path,$str)=@_;
+    my $out= xopen_write($path);
+    binmode $out, ":utf8" or die;
+    xprint_object ($out, $str);
+    $out->xclose;
+}
+
+sub xcopyfile_utf8 ($$) {
+    my ($src,$dest)=@_;
+    xputfile_utf8 ($dest, xgetfile_utf8 ($src));
+}
+
+
+1
