3b08df21ff8ba8057f06e0e659d7dd5b1ab92b92
Christian Jaeger <ch@christianjaeger.ch>
2015-02-06 11:43:49 +0100

    noTEST: update documentation

--- lib/Chj/noTEST.pm
+++ lib/Chj/noTEST.pm
@@ -9,16 +9,18 @@ Chj::noTEST
 
 =head1 SYNOPSIS
 
- use Chj::noTEST;
+ use Chj::noTEST; # instead of `use Chj::TEST;`
  TEST { foo } "bar"; # will be ignored / garbage collected right away
 
 =head1 DESCRIPTION
 
-Disable TEST forms within a package, so that they don't take up memory
-or clutter your test output. (Note that you can always restrict which
-packages to run tests on by giving arguments to run_tests, though. But
-they *will* take up some memory even when never used, hence
-Chj::noTEST.)
+Disable TEST and TEST_STDOUT forms within a package, perhaps because
+they currently fail or are slow, or so that they never use memory.
+
+Note that you can alternatively ignore *all* test forms within the
+whole program by setting the TEST environment variable to 0. Also you
+can pass package names to `run_tests` to limit the tests to run to
+those within the given packages.
 
 =head1 SEE ALSO
 
