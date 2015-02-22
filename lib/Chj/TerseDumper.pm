08c1665ca4b2116845f68306dab5dcd4d4e63c9b
Christian Jaeger <ch@christianjaeger.ch>
2015-02-22 22:55:30 +0100

    create TerseDumper

---
+++ lib/Chj/TerseDumper.pm
@@ -0,0 +1,37 @@
+#
+# Copyright 2015 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+Chj::TerseDumper
+
+=head1 SYNOPSIS
+
+ use Chj::TerseDumper;
+ print TerseDumper($foo);
+
+=head1 DESCRIPTION
+
+Runs Data::Dumper's Dumper with $Data::Dumper::Terse set to 1.
+
+=cut
+
+
+package Chj::TerseDumper;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(TerseDumper);
+@EXPORT_OK=qw();
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict; use warnings FATAL => 'uninitialized';
+
+use Data::Dumper;
+
+sub TerseDumper {
+    local $Data::Dumper::Terse= 1;
+    Dumper(@_)
+}
+
+1
