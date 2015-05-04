72ef1bae965e98f5308c2c8294026beb0ae09432
Christian Jaeger <ch@christianjaeger.ch>
2015-05-04 02:01:03 +0100

    move memory limit setting to testmem.pl

---
+++ testmem.pl
@@ -0,0 +1,14 @@
+use BSD::Resource;
+
+sub MB ($) {
+    $_[0]* 1048576
+}
+
+sub setlimit_mem_MB ($) {
+    my ($limit_MB)=@_;
+    my $limit= MB $limit_MB;
+    setrlimit RLIMIT_VMEM, $limit, $limit
+      or die "setrlimit: $!";
+}
+
+1
