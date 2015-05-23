273211fc58ae2987ad51e02f68b3cef32ee2fcf7
Christian Jaeger <ch@christianjaeger.ch>
2015-05-23 14:00:42 +0100

    create Chj::pp

---
+++ lib/Chj/pp.pm
@@ -0,0 +1,52 @@
+#
+# Copyright 2015 by Christian Jaeger, ch at christianjaeger ch
+# This is free software, offered under the terms of the MIT License.
+# See the file COPYING that came bundled with this file.
+#
+
+=head1 NAME
+
+Chj::pp -- pretty printing as a debugging help
+
+=head1 SYNOPSIS
+
+ use Chj::pp;
+
+ print pp (1/2) + 1, "\n"; # prints "0.5\n" to stderr then "1.5\n" to stdout
+
+ print pp_ ("x", 1/2) + 1, "\n"; # prints "x: 0.5\n" to stderr then see above
+
+=head1 DESCRIPTION
+
+
+=cut
+
+
+package Chj::pp;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(pp pp_);
+@EXPORT_OK=qw();
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict; use warnings FATAL => 'uninitialized';
+
+use Data::Dumper;
+use Chj::TerseDumper;
+
+sub Dump {
+    @_ > 1 ? Dumper(@_) : TerseDumper(@_)
+}
+
+sub pp {
+    print STDERR Dump (@_);
+    wantarray ? @_ : $_[-1]
+}
+
+sub pp_ {
+    my $msg=shift;
+    print STDERR "$msg:", (@_>1 ? "\n" : " "), Dump (@_);
+    wantarray ? @_ : $_[-1]
+}
+
+
+1
