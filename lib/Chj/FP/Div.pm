66226c9a336feb1ab78d11f684fb59e748ac360d
Christian Jaeger <ch@christianjaeger.ch>
2015-05-23 12:03:38 +0100

    add `compose_scalar` and tests

--- lib/Chj/FP/Div.pm
+++ lib/Chj/FP/Div.pm
@@ -19,10 +19,11 @@ Chj::FP::Div
 package Chj::FP::Div;
 @ISA="Exporter"; require Exporter;
 @EXPORT=qw();
-@EXPORT_OK=qw(identity inc dec compose);
+@EXPORT_OK=qw(identity inc dec compose compose_scalar);
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
+use Chj::TEST;
 
 sub identity ($) {
     $_[0]
@@ -40,12 +41,41 @@ sub compose {
     my (@fn)= reverse @_;
     sub {
 	my (@v)= @_;
-	for (@fn) {
-	    @v= &$_(@v);
+	for my $fn (@fn) {
+	    @v= &$fn(@v);
 	}
 	wantarray ? @v : $v[-1]
     }
 }
 
+# same as compose, but request scalar context between the calls
+sub compose_scalar {
+    my (@fn)= reverse @_;
+    my $f0= pop @fn;
+    my $fx= shift @fn;
+    sub {
+	my $v= &$fx;
+	for my $fn (@fn) {
+	    $v= &$fn($v);
+	}
+	@_=($v); goto $f0
+    }
+}
+
+TEST { compose (sub { $_[0]+1 }, sub { $_[0]+$_[1] })->(2,3) }
+  6;
+TEST { compose_scalar  (sub { $_[0]+1 }, sub { $_[0]+$_[1] })->(2,3) }
+  6;
+
+TEST { compose (sub { $_[0] / ($_[1]//5) },
+		sub { @_ },
+		sub { $_[1], $_[0] })
+	 ->(2,3) }
+  1.5;
+TEST { compose_scalar (sub { $_[0] / ($_[1]//5) },
+		       sub { @_ },
+		       sub { $_[1], $_[0] })
+	 ->(2,3) }
+  1/5;
 
 1
