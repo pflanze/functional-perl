4b3713489458e83d264f1f7cc533c21fb0d3565e
Christian Jaeger <ch@christianjaeger.ch>
2015-06-02 20:06:48 +0100

    htmlgen: adapt config to functional-perl project

--- htmlgen/gen-config.pl
+++ htmlgen/gen-config.pl
@@ -2,30 +2,6 @@ use strict; use warnings FATAL => 'uninitialized';
 use Function::Parameters qw(:strict);
 our $mydir; # 'import' from main
 
-my $usageanchor= 'Run `./ml2json --help`.';
-use Chj::xpipe;
-sub helptext {
-    my ($path,$path0)=@_;
-    ## (ugly again)
-    my $diff= substr ($path, 0, length ($path) - length( $path0));
-    my ($r,$w)= xpipe;
-    if (xfork) {
-	$w->xclose;
-	my $str=$r->xcontent;
-	xwait;
-	warn "error in subprocess, exit code $?, output: $str "
-	    unless $? == 0;
-	$str
-    } else {
-	$r->xclose;
-	local $ENV{ML2JSON_MYDIR}=".";
-	open STDOUT, ">&".fileno($w) or die $!;
-	open STDERR, ">&".fileno($w) or die $!;
-	xchdir $diff if length $diff;
-	xexec "./ml2json","--help";
-    }
-}
-
 use PXML::XHTML ":all";
 
 my $logocfg= require "$mydir/logo.pl";
@@ -38,22 +14,12 @@ my $logocfg= require "$mydir/logo.pl";
   },
   path0_handlers=>
   +{
-    "docs/usage.md"=> fun ($path,$path0,$str) {
-	$str=~ s{\Q$usageanchor}{
-	    my $str= helptext($path,$path0);
-	    $str=~ s/^/    /mg;
-	    ("Skip to [instructions](#Instructions) below to see a recipe.\n\n".
-	     "    \$ ./ml2json --help\n".
-	     $str)
-	}e or warn "no match";
-	$str
-    }
    },
   title=> fun ($filetitle) {
-      ($filetitle, " - ml2json")
+      ($filetitle, " - functional-perl.org")
   },
   head=> fun ($path0) {
-      # HTML before navigation
+      # HTML above navigation
       $$logocfg{logo}
   },
   belownav=> fun ($path0) {
@@ -62,18 +28,11 @@ my $logocfg= require "$mydir/logo.pl";
   },
   sortorder=>
   [qw(
-	 README.md
-	 INSTALL.md
-	 docs/usage.md
-	 docs/phases.md
-	 docs/message_identification.md
-	 docs/warnings.md
-	 TODO.md
-	 docs/hacking.md
-	 docs/mbox.md
-	 COPYING.md
-	 CONTACT.md
-	 docs/mailing_list.md
+README.md
+docs/howto_and_comparison_to_Scheme.md
+docs/ideas.md
+examples/README.md
+ftemplate/README.md
     )],
 
   warn_hint=> 1, # warn if the website hint (header) is missing in a
--- htmlgen/logo.pl
+++ htmlgen/logo.pl
@@ -1,12 +1,12 @@
 use strict; use warnings FATAL => 'uninitialized';
 use PXML::XHTML ":all";
 
-my $homeurl= "http://ml2json.christianjaeger.ch";
+my $homeurl= "http://functional-perl.org";
 
 +{
   homeurl=> $homeurl,
   logo=> DIV ({class=> "header"},
 	      A({href=> "$homeurl", class=> "header"},
-		SPAN({class=> "logo1"}, "ml2json"),
-		SPAN({class=> "logo2"}, " mail archive processor"))),
+		SPAN({class=> "logo1"}, "Functional Perl"),
+		SPAN({class=> "logo2"}, " programming project"))),
  }
