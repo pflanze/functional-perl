9f68e7b6ef9ec3f82c29013c37619f658c68931f
Christian Jaeger <ch@christianjaeger.ch>
2015-06-18 08:22:54 +0100

    move `is_filehandle` to `Chj::BuiltinTypePredicates`

---
+++ lib/Chj/BuiltinTypePredicates.pm
@@ -0,0 +1,74 @@
+#
+# Copyright 2015 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+Chj::BuiltinTypePredicates
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Predicates that are useful/needed outside of the `FP` namespace
+tree. Also, to avoid circular dependency on Chj::TEST.
+
+=head1 SEE ALSO
+
+L<FP::Predicates>
+
+=cut
+
+
+package Chj::BuiltinTypePredicates;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(is_filehandle);
+@EXPORT_OK=qw();
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict; use warnings FATAL => 'uninitialized';
+
+use Scalar::Util 'reftype';
+
+
+# for tests, see FP::Predicates
+
+sub is_filehandle ($) {
+    my ($v)=@_;
+    # NOTE: never returns true for strings, even though plain strings
+    # naming globals containing filehandles in their IO slot will work
+    # for IO, too! Let's just leave that depreciated and
+    # 'non-working', ok?
+
+    # NOTE 2: also this only returns true for *references* to globs,
+    # not globs themselves (which could also be used as in
+    # `(*STDOUT)->print( "Huh\n")`). Let's just leave bare globs as
+    # buckets for any of the variable types perl has, and not assume
+    # it's meant to be a filehandle, ok? (Or is that inconsistent with
+    # treating `\*STDOUT` as filehandle? But there's no way around
+    # this one, as that's what `open my $out, ..` gives, and we do
+    # check that the IO slot is actually set in this case.) (hm could
+    # take reference to the bare glob and treat it the same then,
+    # though; but still.)
+
+    if (defined (my $rt= reftype ($v))) {
+	(($rt eq "GLOB" and *{$v}{IO})
+	 or
+	 $rt eq "IO") ? 1 : '';
+	# explicitely return '' instead of undef
+    } else {
+	''
+    }
+}
+
+# sub is_filehandle ($) {
+#     my ($v)=@_;
+#     my $r= ref ($v);
+#     (length $r and ($r eq "GLOB" ? (*{$v}{IO} ? 1 : '')
+# 		    : UNIVERSAL::isa($v, "IO"))) ? 1 : ''
+# }
+# fails for bless $in, "MightActullyBeIO" case
+
+
+1
--- lib/FP/Predicates.pm
+++ lib/FP/Predicates.pm
@@ -84,9 +84,11 @@ package FP::Predicates;
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
-use Scalar::Util 'reftype';
 use Chj::TEST;
 use FP::Pure;
+use Chj::BuiltinTypePredicates 'is_filehandle';
+# ^ should probably move more lowlevel predicates there
+
 
 sub is_pure ($) {
     # XX also treat numbers (or booleans, hum) as pure? But then by
@@ -213,41 +215,7 @@ sub is_instance_of ($) {
 }
 
 
-sub is_filehandle ($) {
-    my ($v)=@_;
-    # NOTE: never returns true for strings, even though plain strings
-    # naming globals containing filehandles in their IO slot will work
-    # for IO, too! Let's just leave that depreciated and
-    # 'non-working', ok?
-
-    # NOTE 2: also this only returns true for *references* to globs,
-    # not globs themselves (which could also be used as in
-    # `(*STDOUT)->print( "Huh\n")`). Let's just leave bare globs as
-    # buckets for any of the variable types perl has, and not assume
-    # it's meant to be a filehandle, ok? (Or is that inconsistent with
-    # treating `\*STDOUT` as filehandle? But there's no way around
-    # this one, as that's what `open my $out, ..` gives, and we do
-    # check that the IO slot is actually set in this case.) (hm could
-    # take reference to the bare glob and treat it the same then,
-    # though; but still.)
-
-    if (defined (my $rt= reftype ($v))) {
-	(($rt eq "GLOB" and *{$v}{IO})
-	 or
-	 $rt eq "IO") ? 1 : '';
-	# explicitely return '' instead of undef
-    } else {
-	''
-    }
-}
-
-# sub is_filehandle ($) {
-#     my ($v)=@_;
-#     my $r= ref ($v);
-#     (length $r and ($r eq "GLOB" ? (*{$v}{IO} ? 1 : '')
-# 		    : UNIVERSAL::isa($v, "IO"))) ? 1 : ''
-# }
-# fails for bless $in, "MightActullyBeIO" case
+# is_filename in Chj::BuiltinTypePredicates
 
 TEST {[ map { is_filehandle $_ }
 	"STDOUT", undef,
