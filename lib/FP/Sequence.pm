e04a7928c7b97872cd808964bf78695f5fe74094
Christian Jaeger <ch@christianjaeger.ch>
2015-06-25 16:08:57 +0100

    Sequence: cleanup, remove commented car/cdr code

--- lib/FP/Sequence.pm
+++ lib/FP/Sequence.pm
@@ -34,10 +34,6 @@ FP sequences are pure (no mutation is allowed, either by force
 (immutability) or by convention (don't offer mutating accessors,
 remind users not to mutate)).
 
-They offer a set of methods that can be overridden. Some methods are
-implemented in terms of others, e.g. `car` is implemented as calling
-`first`, so overriding `first` is enough to cover both (and more).
-
 XX This is a work in progress. More base implementations should be
 moved here, etc.
 
@@ -48,17 +44,9 @@ package FP::Sequence;
 
 use strict; use warnings FATAL => 'uninitialized';
 
-use FP::Pure;
-our @ISA= qw(FP::Pure);
-
-#use FP::Ops "the_method";
+use base 'FP::Pure';
 
 use Chj::NamespaceCleanAbove;
-
-#*car= the_method "first";
-#*cdr= the_method "rest";
-# XX but should these even be part of the API? Perhaps not: only Pairs
-# will support them then. As makes sense!
 
 
 _END_
