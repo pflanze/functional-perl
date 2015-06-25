53d5cd851e587527141468c1198acd1bce21c4e8
Christian Jaeger <ch@christianjaeger.ch>
2015-06-25 16:10:26 +0100

    Sequence: add tests (in separate module)

---
+++ lib/FP/Sequence/t.pm
@@ -0,0 +1,44 @@
+#
+# Copyright 2015 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+FP::Sequence::t -- tests for FP::Sequence
+
+=head1 SYNOPSIS
+
+ # is tested by `t/require_and_run_tests`
+
+=head1 DESCRIPTION
+
+
+=cut
+
+
+package FP::Sequence::t;
+
+use strict; use warnings FATAL => 'uninitialized';
+
+ use FP::Predicates "is_sequence"; # since we can't have it in
+                                   # FP::Sequence
+ use FP::PureArray;
+ use FP::StrictList;
+ use FP::List;
+ use FP::Stream;
+
+ use FP::TEST;
+
+ TEST { list(purearray(3,4),
+             strictlist(3,4),
+             list(3,4),
+             stream(3,4),
+             cons(3,4), # ok this can't really count as a sequence,
+                        # what to do about it?
+             array(3,4), # Would `autobox` change this?
+        )->map(*is_sequence)->array }
+  [ 1,1,1,1,1,'' ];
+
+
+1
