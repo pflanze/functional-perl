b67daabcab119e211860a8cda8223ecff43d0785
Christian Jaeger <ch@christianjaeger.ch>
2015-07-04 22:04:23 +0100

    add `Chj::Trapl`

---
+++ lib/Chj/Trapl.pm
@@ -0,0 +1,36 @@
+#
+# Copyright 2015 by Christian Jaeger, ch at christianjaeger ch
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+Chj::Trapl
+
+=head1 SYNOPSIS
+
+ use Chj::Trapl;
+ die "fun"; # opens a repl from Chj::repl
+
+=head1 DESCRIPTION
+
+Dead-simple wrapper around Chj::WithRepl that simply enables trapping
+globally.
+
+NOTE: the name is not set in stone yet, also, perhaps it should be
+*merged* with Chj::WithRepl.
+
+=head1 SEE ALSO
+
+L<Chj::WithRepl>
+
+=cut
+
+
+package Chj::Trapl;
+
+use Chj::WithRepl;
+
+push_withrepl (0);
+
+1
