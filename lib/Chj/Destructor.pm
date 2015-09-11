7d85629cf9b2de78cfa15164a36a9703ed788f32
Christian Jaeger <ch@christianjaeger.ch>
2015-09-11 21:21:28 +0100

    create Chj::Destructor

---
+++ lib/Chj/Destructor.pm
@@ -0,0 +1,65 @@
+#
+# Copyright (c) 2015 Christian Jaeger, copying@christianjaeger.ch
+#
+# This is free software, offered under either the same terms as perl 5
+# or the terms of the Artistic License version 2 or the terms of the
+# MIT License (Expat version). See the file COPYING.md that came
+# bundled with this file.
+#
+
+=head1 NAME
+
+Chj::Destructor
+
+=head1 SYNOPSIS
+
+ use Chj::Destructor;
+
+ my $z=0;
+ {
+    my $x= ["foo", Destructor { $z++ }];
+ }
+ # $z==1
+
+=head1 DESCRIPTION
+
+Util to help debug or test memory deallocation.
+
+=cut
+
+
+package Chj::Destructor;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(Destructor);
+@EXPORT_OK=qw();
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict; use warnings FATAL => 'uninitialized';
+
+{
+    package Chj::Destructor::_;
+    use FP::Predicates ":all";
+    use FP::Struct [[*is_procedure, "thunk"]];
+    sub DESTROY {
+	my ($self)=@_;
+	$self->thunk->()
+    }
+    _END_
+}
+
+sub Destructor (&) {
+    Chj::Destructor::_->new ($_[0])
+}
+
+use Chj::TEST;
+
+TEST {
+    my $z=0;
+    {
+	my $x= ["foo", Destructor { $z++ }];
+    }
+    $z
+} 1;
+
+
+1
