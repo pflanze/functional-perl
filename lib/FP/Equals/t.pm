9f99892f2fa314f3b3c9b5d0348e49106773ca7e
Christian Jaeger <ch@christianjaeger.ch>
2015-09-11 14:58:25 +0100

    Equals: even more tests

--- lib/FP/Equals/t.pm
+++ lib/FP/Equals/t.pm
@@ -27,6 +27,27 @@ use strict; use warnings FATAL => 'uninitialized';
 use FP::Equals;
 use Chj::TEST;
 
+# compare arguments both straight and swapped; if the results don't
+# match, return an array with both results
+sub tequals {
+    @_==2 or die "wrong num arg";
+    my $x= equals (@_);
+    my $y= equals (@_);
+    if (defined $x) {
+	if (defined $y) {
+	    if ($x eq $y) {
+		return $x
+	    }
+	}
+    } else {
+	if (defined $y) {
+	    [$x, $y]
+	} else {
+	    undef
+	}
+    }
+}
+
 # from the synopsis
 TEST{ equals [1, [2, 3]], [1, [1+1, 3]] } 1;
 TEST{ equals [1, [2, 3]], [1, [1+2, 3]] } '';
@@ -61,8 +82,8 @@ TEST{equals "a", "a"} 1;
 
 # Perl just can't disambiguate between numbers and strings, don't try
 # to fight it?
-TEST{equals "2", 2} 1;
-TEST{equals "2.0", 2.0} '';
+TEST{tequals "2", 2} 1;
+TEST{tequals "2.0", 2.0} '';
 
 my $inf= $^V->{version}[1] > 20 ?
   # XX where exactly was it changed?
@@ -70,43 +91,43 @@ my $inf= $^V->{version}[1] > 20 ?
   # v5.21.11-27-g57e8809: Inf
   "Inf" : "inf";
 
-TEST{equals 1e+20000, $inf} 1;
+TEST{tequals 1e+20000, $inf} 1;
 TEST{ 1e+20000 == "inf" } 1;
-TEST{equals 1/(-1e+2000), 1/(1e+2000) } 1;
+TEST{tequals 1/(-1e+2000), 1/(1e+2000) } 1;
 TEST{ 1/(-1e+2000) == 1/(1e+2000) } 1;
 # so, no need to have both eq and == for those cases!
 
 # but that's not the case here, of course:
 TEST{ -1e1000 == "-1e1000" } 1;
 TEST{ -1e1000 eq "-1e1000" } '';
-TEST{ equals -1e1000, "-1e1000" } '';
-TEST{ equals -1e1000, "-$inf" } 1;
+TEST{ tequals -1e1000, "-1e1000" } '';
+TEST{ tequals -1e1000, "-$inf" } 1;
 TEST{ -1e1000 == "-inf" } 1;
 
-TEST{equals 2, 2.0} 1;	   # those are converted to the same value at
+TEST{tequals 2, 2.0} 1;	   # those are converted to the same value at
                            # compile time.
-TEST{equals "2", "2.0"} '';
+TEST{tequals "2", "2.0"} '';
 
 # Weak typing is where the data (or context of the language) doesn't
 # say what it is.
 
-TEST{equals \("foo"), \("f"."oo")} 1;
-TEST{equals \("foo"), \("bar")} '';
+TEST{tequals \("foo"), \("f"."oo")} 1;
+TEST{tequals \("foo"), \("bar")} '';
 TEST{my $x= undef;
      my $y= undef;
-     equals \$x, \$y} 1;
+     tequals \$x, \$y} 1;
 TEST{my $x= "foo";
      my $y= undef;
-     equals \$x, \$y} undef;
+     tequals \$x, \$y} undef;
 
 # globs
-TEST{equals *foo::bar, "*foo::bar"} undef;
-TEST{equals ((*foo::bar)."", "*foo::bar")} 1;
-TEST{equals *foo, *FP::Equals::t::foo} 1;
-TEST{equals \*foo, \*FP::Equals::t::foo} 1;
-TEST{equals \(*foo::bar), \("*foo::bar")} undef;
-TEST{equals \(*foo::bar), \(*foo::baz)} '';
-TEST{equals *foo, *bar} '';
+TEST{tequals *foo::bar, "*foo::bar"} undef;
+TEST{tequals ((*foo::bar)."", "*foo::bar")} 1;
+TEST{tequals *foo, *FP::Equals::t::foo} 1;
+TEST{tequals \*foo, \*FP::Equals::t::foo} 1;
+TEST{tequals \(*foo::bar), \("*foo::bar")} undef;
+TEST{tequals \(*foo::bar), \(*foo::baz)} '';
+TEST{tequals *foo, *bar} '';
 
 
 # filehandles
@@ -132,32 +153,43 @@ use FP::Stream;
 use FP::List;
 use FP::Lazy;
 
-TEST {equals "a", lazy { chr 65+32 } } 1;
-TEST {equals stream (1,2), stream (1,2)} 1;
-TEST {equals stream (1,2), lazy { cons 1, stream (2)}} 1;
-TEST {equals stream (1,2), cons 1, stream (2)} 1;
+TEST {tequals "a", lazy { chr 65+32 } } 1;
+TEST {tequals stream (1,2), stream (1,2)} 1;
+TEST {tequals stream (1,2), lazy { cons 1, stream (2)}} 1;
+TEST {tequals stream (1,2), cons 1, stream (2)} 1;
 
 # only one of the arguments lazy:
 
-TEST { equals lazy { 2+1 }, 1+2 } 1;
-TEST { equals 1+2, lazy { 2+1 } } 1;
+TEST { tequals lazy { 2+1 }, 1+2 } 1;
 
-TEST { equals lazy { [2+1] }, [lazy { 1+2 }] } 1;
-TEST { equals lazy { [2+1] }, [lazy { 1+3 }] } '';
+TEST { tequals lazy { [2+1] }, [lazy { 1+2 }] } 1;
+TEST { tequals lazy { [2+1] }, [lazy { 1+3 }] } '';
 
-TEST { equals lazy { 2+1 }, [1+2] } undef;
-TEST { equals lazy { [2+1] }, 1+2 } undef;
-# and swapped arguments (shouldn't do this manually):
-TEST { equals [1+2], lazy { 2+1 } } undef;
-TEST { equals 1+2, lazy { [2+1] } } undef;
+TEST { tequals lazy { 2+1 }, [1+2] } undef;
+TEST { tequals lazy { [2+1] }, 1+2 } undef;
 
 # does it force identical promises?
 
 my ($a,$b,$sideeffect);
-TEST { $a= lazy { $sideeffect++; 33*3 };
+TEST { $sideeffect= 0;
+       $a= lazy { $sideeffect++; 33*3 };
        $b= $a;
-       equals $a, $b
+       tequals $a, $b
    } 1;
-TEST { $sideeffect } undef;
+TEST { $sideeffect } 0;
+
+TEST { $sideeffect= 0;
+       $a= lazy { $sideeffect++; 33*3 };
+       $b= lazy { $sideeffect++; 33*3 };
+       tequals $a, $b
+   } 1;
+TEST { $sideeffect } 2;
+
+# lazy and undef or non-references:
+
+TEST { tequals lazy { undef }, undef } 1;
+TEST { tequals lazy { "a" }, "a" } 1;
+TEST { tequals lazy { "a" }, undef } undef;
+TEST { tequals lazy { undef }, "a" } undef;
 
 1
