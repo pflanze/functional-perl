24f17d544f018405716c59a4eaf7b2179fbb409c
Christian Jaeger <ch@christianjaeger.ch>
2015-10-09 18:31:23 +0100

    AskYN: adapt to new license, slightly modernize the code

--- lib/Chj/Util/AskYN.pm
+++ lib/Chj/Util/AskYN.pm
@@ -1,9 +1,11 @@
-# Sun Jun 22 01:15:07 2003  Christian Jaeger, christian.jaeger@ethlife.ethz.ch
-# 
-# Copyright 2001 by ethlife renovation project people
-# Published under the terms of the GNU General Public License
 #
-# $Id$
+# Copyright (c) 2003-2015 Christian Jaeger, copying@christianjaeger.ch
+#
+# This is free software, offered under either the same terms as perl 5
+# or the terms of the Artistic License version 2 or the terms of the
+# MIT License (Expat version). See the file COPYING.md that came
+# bundled with this file.
+#
 
 =head1 NAME
 
@@ -11,20 +13,35 @@ Chj::Util::AskYN
 
 =head1 SYNOPSIS
 
+ use Chj::Util::AskYN;
+
+ LP: {
+     if (maybe_askyn "Do you want to retry?") {
+         redo LP;
+     }
+ }
+
 =head1 DESCRIPTION
 
+Simply ask for a boolean question on stdout/stdin. Accept y/n, yes/no
+in english, german and french and return those as boolean
+true/false. If the user closes the input (using ctl-d), undef is
+returned.
+
+=head1 TODO
+
+Delete this and use something else?
 
 =cut
 
 
 package Chj::Util::AskYN;
 @ISA="Exporter"; require Exporter;
-@EXPORT= qw(askyn);
-
+@EXPORT= qw(maybe_askyn);
 
 use strict;
 
-sub askyn {
+sub maybe_askyn {
     my ($prompt)=@_;
   ASK:{
 	if (defined $prompt) {
@@ -32,18 +49,20 @@ sub askyn {
 	    print $prompt;
 	}
 	my $ans=<STDIN>;
-	if (! $ans) {
-	    print "\n";# enter nachholen damit die shell zeile nicht auf derselben zeile landet.
-	    return # undef!
-	}
-	if ($ans=~ /^n(?:o|ein|ada|on)?$/i) {
-	    return 0;
-	} elsif ($ans=~ /^(?:ja|yes|j|y|oui)$/i){
-	    return 1;
+	if (defined $ans) {
+	    if ($ans=~ /^n(?:o|ein|ada|on)?$/i) {
+		''
+	    } elsif ($ans=~ /^(?:ja|yes|j|y|oui)$/i){
+		1
+	    } else {
+		redo ASK;
+	    }
 	} else {
-	    redo ASK;
+	    # EOF, i.e. ctl-d
+	    print "\n";
+	    undef
 	}
     }
 }
 
-1;
+1
