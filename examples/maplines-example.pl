1d3868f7056cb4fd73abd3e56a76422f11c6e2a2
Christian Jaeger <ch@christianjaeger.ch>
2016-06-12 18:11:14 +0100

    examples/maplines: change config into hashtable, take sort comparison fn

--- examples/maplines-example.pl
+++ examples/maplines-example.pl
@@ -1,16 +1,23 @@
 use strict; use warnings; use warnings FATAL => 'uninitialized';
 
+use FP::Array_sort qw(on);
+use FP::Ops qw(string_lc string_cmp);
+
+
 my $m= +{
   'ch@christianjaeger.ch'=> "Christian Jaeger",
   'foo@example.com'=> "Mr Example",
   'baz@example.com'=> undef, # drop
  };
 
-sub {
-    my ($addr)=@_;
-    if (exists $$m{$addr}) {
-	$$m{$addr}
-    } else {
-	die "unknown address: '$addr'";
-    }
-}
++{
+  map=> sub {
+      my ($addr)=@_;
+      if (exists $$m{$addr}) {
+	  $$m{$addr}
+      } else {
+	  die "unknown address: '$addr'";
+      }
+  },
+  cmp=> on (*string_lc, *string_cmp),
+ }
