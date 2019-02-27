2efa08a0798cde5484fd1c9ed0ee30a34f4c398f
Christian Jaeger <ch@christianjaeger.ch>
2019-02-27 17:27:00 +0000

    Nav: can't use weakened here
    Why did that work on other Perl versions, though, didn't it?

--- lib/FP/Stream.pm
+++ lib/FP/Stream.pm
@@ -968,6 +968,7 @@ sub stream_cartesian_product_2 {
 	my ($a,$b)=@_;
 	lazy {
 	    if (is_null $a) {
+                undef $rec; # see X... comment below
 		null
 	    } elsif (is_null $b) {
 		&$rec (cdr $a, $orig_b);
@@ -977,7 +978,9 @@ sub stream_cartesian_product_2 {
 	    }
 	}
     };
-    Weakened($rec)->($a, $orig_b)
+    # Weakened($rec)->($a, $orig_b)
+    # XXX is it impossible not to leak safely?
+    $rec->($a, $orig_b)
 }
 
 *FP::List::List::stream_cartesian_product_2= *stream_cartesian_product_2;
