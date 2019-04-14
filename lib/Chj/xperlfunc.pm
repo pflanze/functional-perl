1345a22d186a45cea164e0672e66d011286821ac
Christian Jaeger <ch@christianjaeger.ch>
2019-04-14 01:56:32 +0100

    update copyright year

--- lib/Chj/xperlfunc.pm
+++ lib/Chj/xperlfunc.pm
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2003-2015 Christian Jaeger, copying@christianjaeger.ch
+# Copyright (c) 2003-2019 Christian Jaeger, copying@christianjaeger.ch
 #
 # This is free software, offered under either the same terms as perl 5
 # or the terms of the Artistic License version 2 or the terms of the
