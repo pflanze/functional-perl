00d7a8a907c9d1264caf6d569d28cd53722d62e1
Christian Jaeger <ch@christianjaeger.ch>
2019-11-11 17:07:26 +0000

    List: test/fix pod snippets

--- lib/FP/List.pm
+++ lib/FP/List.pm
@@ -13,12 +13,12 @@ FP::List - singly linked (purely functional) lists
 
 =head1 SYNOPSIS
 
+ use FP::Div qw(inc square); use FP::Ops qw(div); use FP::Equal 'is_equal';
  use FP::List ':all';
- use FP::Div qw(inc square);
- use FP::Ops qw(div);
 
- is list_to_string(cons("H",cons("e",cons("l",cons("l",cons("o",null)))))),
-    "Hello";
+ my $l= cons("H",cons("e",cons("l",cons("l",cons("o",null)))));
+ is_equal $l, list("H", "e", "l", "l", "o");
+ is_equal list_to_string($l), "Hello";
 
  is_equal list(1,2,3)->map(sub{ $_[0] * $_[0] }),
           list (1,4,9);
@@ -29,12 +29,10 @@ FP::List - singly linked (purely functional) lists
  is_equal list(qw(a b c))->rest,
           list("b", "c");
 
- is list(1,2,3,4)->sum, 10;             # (1+2+3+4)
- is list(1,2,3,4)->product, 24;         # (1*2*3*4)
- is list(2,4)->reduce(*div), 2;         # (4/2)
- is list(2,4,6)->reduce(*div), 3;       # (6/(4/2))
- is list(2,4,6)->reduce_right(*div), 3; # (6/(4/2))
- is list(2,4,6)->reduce_right(*div), 1.3333; # XXXX. (
+ is list(1,2,3,4)->sum, 1+2+3+4;
+ is list(1,2,3,4)->product, 1*2*3*4;
+ is list(2,4,6)->reduce(flip *div), 2/4/6;
+ is list(2,4,6)->reduce_right(flip *div), 2/4/6;
  # etc.
 
  # The `cons` function checks whether its second argument is an object
