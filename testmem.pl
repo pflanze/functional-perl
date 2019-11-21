4f929fbdacb4535a9afb088f0d0a68afab023746
Christian Jaeger <ch@christianjaeger.ch>
2019-11-21 19:17:04 +0000

    testmem.pl: no RLIMIT_VMEM on OpenBSD

--- testmem.pl
+++ testmem.pl
@@ -5,10 +5,16 @@ sub MB ($) {
     $_[0]* 1048576
 }
 
+my $RLIMIT_KIND=
+    # At least OpenBSD does not have RLIMIT_VMEM:
+    $^O =~ /bsd/i ? RLIMIT_DATA() # openbsd, freebsd, netbsd?
+    # others, including Linux:
+    : RLIMIT_VMEM();
+    
 sub setlimit_mem_MB ($) {
     my ($limit_MB)=@_;
     my $limit= MB $limit_MB;
-    setrlimit RLIMIT_VMEM, $limit, $limit
+    setrlimit $RLIMIT_KIND, $limit, $limit
       or die "setrlimit: $!";
 }
 
