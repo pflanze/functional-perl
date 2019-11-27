37564fc97436a64c445c668f2d302009d538579b
Christian Jaeger <ch@christianjaeger.ch>
2019-11-27 02:16:18 +0000

    New release

--- lib/FunctionalPerl.pm
+++ lib/FunctionalPerl.pm
@@ -128,7 +128,7 @@ package FunctionalPerl;
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
 
-our $VERSION= "0.72.11";
+our $VERSION= "0.72.12";
 
 
 # Export tag to modules and/or other tags; each module will be
