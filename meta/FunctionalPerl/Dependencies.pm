0187c99a6d7ff53cf5bdd46bafd01f1eab86fea3
Christian Jaeger <ch@christianjaeger.ch>
2019-11-30 15:05:43 +0000

    Dependencies: fix 'Chj::Serialize' entry

--- meta/FunctionalPerl/Dependencies.pm
+++ meta/FunctionalPerl/Dependencies.pm
@@ -41,6 +41,7 @@ our %dependencies=
   (
    # Don't specify Sub::Call::Tail (if meta/tail-expand can expand it)!
 
+   'Chj::Serialize'=> ['FP::Repl::Dependencies', 'B::Deparse'],
    'FP::Docstring'=> ['B::Deparse'],
    'FP::BigInt'=> ['Math::BigInt'],
    'FP::autobox'=> ['autobox'],
@@ -107,7 +108,6 @@ our %dependencies=
          FP::Repl::Dependencies
          FP::Repl::Repl
          FP::Repl::StackPlus
-         Chj::Serialize
          FP::Repl::Trap
          FP::Repl::WithRepl
          FP::Repl
