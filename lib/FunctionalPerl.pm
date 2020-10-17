44fab5686b9dcfc41865ff3e121bde0fffb0c3b3
Christian Jaeger <ch@christianjaeger.ch>
2020-10-17 03:29:25 +0100

    Release 0.72.34

--- lib/FunctionalPerl.pm
+++ lib/FunctionalPerl.pm
@@ -133,7 +133,7 @@ package FunctionalPerl;
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
 
-our $VERSION= "0.72.33";
+our $VERSION= "0.72.34";
 
 
 # Export tag to modules and/or other tags; each module will be
