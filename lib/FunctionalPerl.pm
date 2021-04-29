8f494f2db383a3569b9d85bd820ff8e09cdb20e8
Christian Jaeger <ch@christianjaeger.ch>
2021-04-29 23:41:10 +0000

    Release 0.72.56

--- lib/FunctionalPerl.pm
+++ lib/FunctionalPerl.pm
@@ -137,7 +137,7 @@ our @EXPORT      = ();
 our @EXPORT_OK   = qw(expand_import_tags);
 our %EXPORT_TAGS = ();
 
-our $VERSION = "0.72.55";
+our $VERSION = "0.72.56";
 
 # Export tag to modules and/or other tags; each module will be
 # imported with ":all" by default. Where a module name contains " = ",
