ad3769788b9c84eab7faa17d774adc254e08a480
Christian Jaeger <ch@christianjaeger.ch>
2021-05-26 16:22:12 +0000

    ModuleList: consistent internal naming

--- meta/FunctionalPerl/ModuleList.pm
+++ meta/FunctionalPerl/ModuleList.pm
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015-2020 Christian Jaeger, copying@christianjaeger.ch
+# Copyright (c) 2015-2021 Christian Jaeger, copying@christianjaeger.ch
 #
 # This is free software, offered under either the same terms as perl 5
 # or the terms of the Artistic License version 2 or the terms of the
@@ -35,10 +35,10 @@ our %EXPORT_TAGS = (all => [@EXPORT, @EXPORT_OK]);
 
 use Chj::xopen 'xopen_read';
 
-our $moduleandpathlist;    # [[ name, path ] ...]
+our $modulenameandpathlist;    # [[ name, path ] ...]
 
-sub moduleandpathlist {
-    $moduleandpathlist //= do {
+sub modulenameandpathlist {
+    $modulenameandpathlist //= do {
         my $f = xopen_read "MANIFEST";
         my @m;
         local $_;
@@ -56,11 +56,11 @@ sub moduleandpathlist {
 }
 
 sub modulenamelist {
-    [map { $$_[0] } @{ moduleandpathlist() }]
+    [map { $$_[0] } @{ modulenameandpathlist() }]
 }
 
 sub modulepathlist {
-    [map { $$_[1] } @{ moduleandpathlist() }]
+    [map { $$_[1] } @{ modulenameandpathlist() }]
 }
 
 1
